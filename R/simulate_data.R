64abe9a295937953d8bc42443a2753bde6cbd588
DongyueXie <dongyxie@gmail.com>
2023-05-07 03:26:18 -0500

    revise simdata function

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -3,12 +3,11 @@
 #'@param Ulist a list of correlation matrices
 #'@param Pi prior weights
 #'@param prior normal, t, uniform
-#'@param half.uniform True to generate only positive uniforms
 #'@param mean.range range of alternative E(z), from -mean.range to mean.range
 #'@importFrom mvtnorm rmvnorm
 #'@importFrom mvtnorm rmvt
 #'@export
-simDataI.ult = function(N,Ulist,Pi=NULL,prior="t",t.df=10,half.uniform=FALSE,mean.range = 4){
+simDataI.ult = function(N,Ulist,Pi=NULL,prior="t",t.df=10,mean.range = 4){
 
   K = length(Ulist)
   R = ncol(Ulist[[1]])
@@ -40,19 +39,16 @@ simDataI.ult = function(N,Ulist,Pi=NULL,prior="t",t.df=10,half.uniform=FALSE,mea
       Bk = rmvu(nsamp[k],Ulist[[k]])
       B = rbind(B,Bk)
     }
-
-    #temp = sqrt(signal_sd^2*12)
-
-    if(half.uniform){
-      B = B*mean.range
-    }else{
-      non0.idx = which(B!=0)
-      B[non0.idx] = B[non0.idx]*2*mean.range-mean.range
+    non0.idx = which(B!=0)
+    B[non0.idx] = B[non0.idx]*2*mean.range-mean.range
+  }else if(prior == 'half.uniform'){
+    for(k in 1:K){
+      Bk = rmvu(nsamp[k],Ulist[[k]])
+      B = rbind(B,Bk)
     }
-
-
+    B = B*mean.range
   }else{
-    stop('prior should be one of: normal, t, uniform')
+    stop('prior should be one of: normal, t, uniform, half.uniform')
   }
 
 
@@ -68,12 +64,12 @@ simDataI.ult = function(N,Ulist,Pi=NULL,prior="t",t.df=10,half.uniform=FALSE,mea
 
 
 
-#'@title generating multivariate uniform distribution
+#'@title generating multivariate uniform(0,1) distribution with correlation U
 rmvu = function(n,U){
   non0.idx = which(diag(U)!=0)
   if(length(non0.idx)==ncol(U)){
     U = cov2cor(U)
-    X = mvtnorm::rmvnorm(n,rep(0,ncol(U)),U)
+    X = rmvnorm(n,rep(0,ncol(U)),U)
     X = apply(X,2,pnorm)
   }else if(length(non0.idx)==0){
     X = matrix(0,nrow=n,ncol=ncol(U))
@@ -81,7 +77,7 @@ rmvu = function(n,U){
     U.non0 = U[non0.idx,non0.idx,drop=F]
     U.non0 = cov2cor(U.non0)
     X = matrix(0,nrow=n,ncol=ncol(U))
-    x = mvtnorm::rmvnorm(n,rep(0,ncol(U.non0)),U.non0)
+    x = rmvnorm(n,rep(0,ncol(U.non0)),U.non0)
     x = apply(x,2,pnorm)
     X[,non0.idx] = x
   }
--- tests/testthat/test_maskedmash.R
+++ tests/testthat/test_maskedmash.R
@@ -21,3 +21,63 @@ fit_maskedmash_onZ_fdr = maskedmashFDR(fit_maskedmash_onZ)
 
 fit_mash = mash_wrapper(datax$Bhat)
 fit_mash_fdr = mashFDR(fit_mash)
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# sim_study = function(Ulist,prior_weight,N,prior,df,
+#                      seed=12345,nreps = 20,mc.cores = 4,npc=5,
+#                      half.uniform=FALSE,mean.range=4){
+#
+#   set.seed(seed)
+#
+#   result = mclapply(1:nreps,function(i){
+#     # generate data
+#     simdata = simDataI.ult(N,Ulist,prior_weight,prior,df,half.uniform,mean.range)
+#     #datax = mash_set_data(simdata$Bhat,simdata$Shat)
+#     # fit model
+#     mash.out = mash_wrapper(simdata$Bhat,npc=npc)
+#
+#     mashonmaskedZ.out = mash_mask(simdata$Bhat,simdata$P,p.thresh = 0.5,npc=npc)
+#     mashonmaskedZ.out2 = mash_mask(simdata$Bhat,simdata$P,p.thresh = 0.4,npc=npc)
+#     mashonmaskedZ.out3 = mash_mask(simdata$Bhat,simdata$P,p.thresh = 0.3,npc=npc)
+#     mashonmaskedZ.out4 = mash_mask(simdata$Bhat,simdata$P,p.thresh = 0.2,npc=npc)
+#     mashonmaskedZ.out5 = mash_mask(simdata$Bhat,simdata$P,p.thresh = 0.1,npc=npc)
+#     mashonmaskedZ.out6 = mash_mask(simdata$Bhat,simdata$P,p.thresh = 0.05,npc=npc)
+#
+#     maskedmash.out = maskedmash_wrapper(simdata$Bhat,simdata$P,p.thresh = 0.5,npc=npc)
+#     maskedmash.out2 = maskedmash_wrapper(simdata$Bhat,simdata$P,p.thresh=0.4,npc=npc)
+#     maskedmash.out3 = maskedmash_wrapper(simdata$Bhat,simdata$P,p.thresh=0.3,npc=npc)
+#     maskedmash.out4 = maskedmash_wrapper(simdata$Bhat,simdata$P,p.thresh=0.2,npc=npc)
+#     maskedmash.out5 = maskedmash_wrapper(simdata$Bhat,simdata$P,p.thresh=0.1,npc=npc)
+#     maskedmash.out6 = maskedmash_wrapper(simdata$Bhat,simdata$P,p.thresh=0.05,npc=npc)
+#
+#     list(data = simdata,
+#          mash.out=mash.out,
+#          mashonmaskedZ.out=mashonmaskedZ.out,
+#          mashonmaskedZ.out2=mashonmaskedZ.out2,
+#          mashonmaskedZ.out3=mashonmaskedZ.out3,
+#          mashonmaskedZ.out4=mashonmaskedZ.out4,
+#          mashonmaskedZ.out5=mashonmaskedZ.out5,
+#          mashonmaskedZ.out6=mashonmaskedZ.out6,
+#          maskedmash.out=maskedmash.out,
+#          maskedmash.out2=maskedmash.out2,
+#          maskedmash.out3=maskedmash.out3,
+#          maskedmash.out4=maskedmash.out4,
+#          maskedmash.out5=maskedmash.out5,
+#          maskedmash.out6=maskedmash.out6)
+#
+#   },mc.cores = mc.cores)
+#
+#   result
+#
+# }
