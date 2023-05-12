c9735c8d12f7d380ec07580a2be8e9819bf11247
DongyueXie <dongyxie@gmail.com>
2023-05-11 23:03:28 -0500

    add teem md

--- R/mash_wrapper.R
+++ R/mash_wrapper.R
@@ -6,13 +6,17 @@
 #'@importFrom mashr mash
 #'@importFrom mashr mash_1by1
 #'@export
-mash_wrapper = function(Bhat,P=NULL,npc=5,Shat=NULL,
-                        nu = ncol(Bhat)+1,verbose=FALSE,
+mash_wrapper = function(Bhat,P=NULL,
+                        npc=5,Shat=NULL,
+                        nu = ncol(Bhat)+1,
+                        verbose=FALSE,
                         adjust = "lb",
+                        md.method = 'teem',
+                        strong.thresh = 0.2,
                         U.true = NULL,
-                        U.ed=NULL,
-                        n.ed=NULL,
-                        adj.const = 0.1){
+                        U.data=NULL,
+                        n.data=NULL,
+                        adj.const = 1e-3){
 
 
 
@@ -30,20 +34,29 @@ mash_wrapper = function(Bhat,P=NULL,npc=5,Shat=NULL,
 
   if(is.null(U.true)){
 
-    if(is.null(U.ed)){
+    if(is.null(U.data)){
 
       m.1by1 = mash_1by1(data)
-      strong = get_significant_results(m.1by1)
-      n.ed = length(strong)
+      strong = get_significant_results(m.1by1,thresh = strong.thresh)
+      n.data = length(strong)
       if(length(strong)==0){
         strong=NULL
-        n.ed=N
+        n.data=N
       }
       U.pca = cov_pca(data,npc,strong)
-      U.ed = mashr:::bovy_wrapper(data,U.pca,subset=strong)
+      if(md.method=='ed'){
+        U.data = mashr:::bovy_wrapper(data,U.pca,subset=strong)
+        U.est = U.data$Ulist
+        pi.est = pi.est
+      }else if(md.method=='teem'){
+        U.data = mashr:::teem_wrapper(data,U.pca,subset=strong)
+        U.est = U.data$U
+        U.est = lapply(seq(dim(U.est)[3]), function(x) U.est[ , , x])
+        pi.est = U.data$w
+      }
+
     }
-    U.est = U.ed$Ulist
-    ##############
+
     ##############
     # U.est  = lapply(U.est,function(x){
     #   x - diag(1/sqrt(N),R)
@@ -54,7 +67,7 @@ mash_wrapper = function(Bhat,P=NULL,npc=5,Shat=NULL,
 
       if(adjust == 'prior'){
         U.est = lapply(1:length(U.est),function(k){
-          nk = n.ed*U.ed$pi[k]
+          nk = n.data*pi.est[k]
           if(nk>R){
             s2.hat = R*nu/(nk+nu)/sum(diag(solve(nk*(U.est[[k]]+diag(R)))))
             (nk/(nk+nu-R-1))*U.est[[k]] + ((R+1-nu+s2.hat)/(nk+nu-R-1))*diag(R)
@@ -67,21 +80,21 @@ mash_wrapper = function(Bhat,P=NULL,npc=5,Shat=NULL,
 
       if(adjust == "lb"){
         U.est = lapply(1:length(U.est),function(k){
-          nk = n.ed*U.ed$pi[k]
+          nk = n.data*pi.est[k]
           if(nk>1){
             U.est[[k]] + 2*diag(sqrt(2/nk),R)
             #U.est[[k]] + 2*diag(sqrt(2/nk*1/(diag(solve(U.est[[k]]+diag(R))))^2))
             #temp = eigen(U.est[[k]])
             #temp$vectors%*%(diag(pmax(2/sqrt(nk),temp$values)))%*%t(temp$vectors)
           }else{
-            NULL
+            U.est[[k]] + 2*diag(sqrt(2/n.data),R)
           }
         })
       }
 
       if(adjust == "const"){
         U.est = lapply(1:length(U.est),function(k){
-          U.est[[k]] + diag(adj.const,R)
+          cov2cor(U.est[[k]]) + diag(adj.const,R)
         })
       }
 
